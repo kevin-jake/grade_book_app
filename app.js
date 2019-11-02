04b0459e4f14c40c3952c3a59d267283c90c9321
kevin-jake <kevin_jake_04@yahoo.com>
2019-11-03 04:04:50 +0800

    - remove multer

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
-const multer = require('multer');
 const app = express();
 
 app.get('/', (req, res) => {
