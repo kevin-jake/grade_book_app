cd1646637e77525e41e975a72fe400498e066c42
Kevin Jake <kevin_jake_04@yahoo.com>
2024-03-01 02:06:55 +0800

    fix error in database and add env variables

--- app.js
+++ app.js
@@ -1,125 +1,127 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
 const app = express();
 const db = require("./db");
-const Joi = require('joi');
+const Joi = require("joi");
 
 const collection = "grades";
-var fromGET = []
-var dbID = {}
-var fromFind = []
+var fromGET = [];
+var dbID = {};
+var fromFind = [];
 
 function isEmptyObject(obj) {
-    return !Object.keys(obj).length;
-  }
+  return !Object.keys(obj).length;
+}
 
 // schema used for data validation for our database data
 const schema = Joi.object().keys({
-    id: Joi.number().required(),
-    fullName: Joi.string(),
-    quarterGrades: Joi.array().items(
-        Joi.object().keys({
-            quarter: Joi.string(),
-            year: Joi.string(),
-            homeworkGrade: Joi.array().items(Joi.number()),
-            testGrade: Joi.array().items(Joi.number()),
-            finalGrade: Joi.number()
-        })
-    )
-
+  id: Joi.number().required(),
+  fullName: Joi.string(),
+  quarterGrades: Joi.array().items(
+    Joi.object().keys({
+      quarter: Joi.string(),
+      year: Joi.string(),
+      homeworkGrade: Joi.array().items(Joi.number()),
+      testGrade: Joi.array().items(Joi.number()),
+      finalGrade: Joi.number(),
+    })
+  ),
 });
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/dataProcess.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dataProcess.js'));
+app.get("/dataProcess.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "dataProcess.js"));
 });
 
-app.get('/getDatafromDB', (req, res) => {
-    db.getDB().collection(collection).find({}).toArray((err, documents) => {
-        if (err)
-            console.log(err);
-        else {
-            fromGET = documents;
-            res.json(documents);
-        }
+app.get("/getDatafromDB", (req, res) => {
+  db.getDB()
+    .collection(collection)
+    .find({})
+    .toArray((err, documents) => {
+      if (err) console.log(err);
+      else {
+        fromGET = documents;
+        res.json(documents);
+      }
     });
 });
 
-app.post('/gradesSave', (req, res, next) => {
-    const userInput = req.body;
+app.post("/gradesSave", (req, res, next) => {
+  const userInput = req.body;
 
-    console.log(userInput)
-    fromGET.forEach(element => {
-        if (element.fullName == userInput.fullName) {
-            dbID = db.getPrimaryKey(element._id);
-            fromFind = element
-            console.log(db.getPrimaryKey(element._id))
-
-        }
-    });
-    Joi.validate(userInput, schema, (err, result) => {
-        if (err) {
-            const error = new Error("Invalid Input");
-            error.status = 400;
-            next(error);
-        }
-        else {
-            if (!isEmptyObject(dbID)) {
-                    db.getDB().collection(collection).findOneAndUpdate(
-                        { _id: db.getPrimaryKey(fromFind._id) },
-                        {
-                            $set: userInput
-                        },
-                        {returnOriginal : false},
-                        (err, result) => {
-                            if (err)
-                                console.log(err);
-                            else {
-                                console.log("Updated on DB")
-                                res.json(result);
-                                return next()
-                            }
-                        });
-
+  console.log(userInput);
+  fromGET.forEach((element) => {
+    if (element.fullName == userInput.fullName) {
+      dbID = db.getPrimaryKey(element._id);
+      fromFind = element;
+      console.log(db.getPrimaryKey(element._id));
+    }
+  });
+  Joi.validate(userInput, schema, (err, result) => {
+    if (err) {
+      const error = new Error("Invalid Input");
+      error.status = 400;
+      next(error);
+    } else {
+      if (!isEmptyObject(dbID)) {
+        db.getDB()
+          .collection(collection)
+          .findOneAndUpdate(
+            { _id: db.getPrimaryKey(fromFind._id) },
+            {
+              $set: userInput,
+            },
+            { returnOriginal: false },
+            (err, result) => {
+              if (err) console.log(err);
+              else {
+                console.log("Updated on DB");
+                res.json(result);
+                return next();
+              }
             }
-            else {
-                db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-                    if (err) {
-                        const error = new Error("Failed to insert Document");
-                        error.status = 400;
-                        next(error);
-                    }
-                    else
-                        res.json({ result: result, document: result.ops[0], msg: "Successfully inserted Grades!!!", error: null });
-                    return next()
-                });
-            }
-
-        }
-    })
-})
+          );
+      } else {
+        db.getDB()
+          .collection(collection)
+          .insertOne(userInput, (err, result) => {
+            if (err) {
+              const error = new Error("Failed to insert Document");
+              error.status = 400;
+              next(error);
+            } else
+              res.json({
+                result: result,
+                document: result.ops[0],
+                msg: "Successfully inserted Grades!!!",
+                error: null,
+              });
+            return next();
+          });
+      }
+    }
+  });
+});
 
 db.connect((err) => {
-    if (err) {
-        console.log('unable to connect to database');
-        process.exit(1);
-    }
-    else {
-        console.log('DB Connected!')
-        app.listen(3000, () => {
-            console.log('App listening on port 3000');
-        });
-    }
+  if (err) {
+    console.log("unable to connect to database");
+    console.log(err);
+    process.exit(1);
+  } else {
+    console.log("DB Connected!");
+    app.listen(3000, () => {
+      console.log("App listening on port 3000");
+    });
+  }
 });
-
-
 
 module.export = app;
--- db.js
+++ db.js
@@ -1,50 +1,47 @@
+require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 // name of our database
 const dbname = "grade_book_app";
 // location of where our mongoDB database is located
-const url = "mongodb+srv://edukasyon:EdukasyonPH@devcluster-bbmyk.mongodb.net/test?retryWrites=true&w=majority";
+const url = `mongodb://${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // Options for mongoDB
 const mongoOptions = {
-    useNewUrlParser : true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const state = {
-    db : null
+  db: null,
 };
 
-const connect = (cb) =>{
-    // if state is not NULL
-    // Means we have connection already, call our CB
-    if(state.db)
+const connect = (cb) => {
+  // if state is not NULL
+  // Means we have connection already, call our CB
+  if (state.db) cb();
+  else {
+    // attempt to get database connection
+    MongoClient.connect(url, mongoOptions, (err, client) => {
+      // unable to get database connection pass error to CB
+      if (err) cb(err);
+      // Successfully got our database connection
+      // Set database connection and call CB
+      else {
+        state.db = client.db(dbname);
         cb();
-    else{
-        // attempt to get database connection
-        MongoClient.connect(url,mongoOptions,(err,client)=>{
-            // unable to get database connection pass error to CB
-            if(err)
-                cb(err);
-            // Successfully got our database connection
-            // Set database connection and call CB
-            else{
-                state.db = client.db(dbname);
-                cb();
-            }
-        });
-    }
-}
+      }
+    });
+  }
+};
 
-// returns OBJECTID object used to 
-const getPrimaryKey = (_id)=>{
-    return ObjectID(_id);
-}
+// returns OBJECTID object used to
+const getPrimaryKey = (_id) => {
+  return ObjectID(_id);
+};
 
-// returns database connection 
-const getDB = ()=>{
-    return state.db;
-}
+// returns database connection
+const getDB = () => {
+  return state.db;
+};
 
-
-
-module.exports = {getDB,connect,getPrimaryKey};
+module.exports = { getDB, connect, getPrimaryKey };
